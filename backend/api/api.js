7bd3e0efd2b753d53d5f35db830a4a0dc0ae4881
sajicode <sajioloye@gmail.com>
2018-08-16 13:29:10 +0100

    link user router to api.js file

--- backend/api/api.js
+++ backend/api/api.js
@@ -1,5 +1,7 @@
 const express = require('express'),
-      api = express.Router();
+      api = express.Router(),
+      userRouter = require('./routers/userRouter');
 
+api.use("/users", userRouter);
 
 module.exports = api;
