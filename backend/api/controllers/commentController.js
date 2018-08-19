5edbad9bad49869d53ed3fd9b64ccb0807e35f8a
sajicode <sajioloye@gmail.com>
2018-08-19 21:53:49 +0100

    add controllers and routes for fetching comments by author and by posts

--- backend/api/controllers/commentController.js
+++ backend/api/controllers/commentController.js
@@ -1,5 +1,6 @@
 const {Comment} = require('../models/commentModel');
 const {Post} = require('../models/postModel');
+const {User} = require('../models/userModel');
 const {ObjectID} = require('mongodb');
 
 exports.addComment = function(req, res) {
@@ -28,6 +29,33 @@ exports.getComment = function(req, res) {
     .then(comment => {
       if(!comment) { return res.status(400).send("Unable to get comment"); }
       res.status(200).send({comment});
+    })
+    .catch(e => res.status(400).send(e));
+};
+
+exports.fetchCommentsByPost = function(req, res) {
+  let postId = req.params.id;
+
+  Post.findById(postId)
+    .populate('comments')
+    .then(post => {
+      if(!post) {
+        return res.status(400).send("Unable to find post")
+      }
+      res.status(200).send(post.comments)
+    })
+    .catch(e => res.status(400).send(e));
+};
+
+exports.fetchCommentsByAuthor = function (req, res) {
+  let userId = req.params.id;
+
+  Comment.find({user: userId})
+    .then(comments => {
+      if(!comments) {
+        return res.status(400).send("Unable to find comments")
+      }
+      res.status(200).send({comments})
     })
     .catch(e => res.status(400).send(e));
 };
--- backend/api/routers/commentRouter.js
+++ backend/api/routers/commentRouter.js
@@ -7,5 +7,11 @@ router.route('/:id')
   .post(authenticate, commentController.addComment)
   .get(authenticate, commentController.getComment)
 
+router.route('/post/:id')
+  .get(authenticate, commentController.fetchCommentsByPost);
+
+router.route('/user/:id')
+  .get(authenticate, commentController.fetchCommentsByAuthor);
+
 
 module.exports = router;
